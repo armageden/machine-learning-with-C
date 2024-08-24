b5f0fdfd5b0c26b740debc68738b9fea19d6c423
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-08-24 22:16:20 +0600

    first start of a new project

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -xe
+clang -Wall -Wextra -o main main.c
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+float train[][2]={
+    {};
+}
+
+int main(){
+    printf("Hello World\n");
+
+    return 0;
+}
