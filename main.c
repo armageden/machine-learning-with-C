346a43e636980bff532736decbf1ed1615a27519
Farhan <loki4444protonmail.com>
2024-08-24 23:26:27 +0600

    1st

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+// this the training data for say...
 float train[][2]={
-    {};
-}
+    {1,2}
+    {2,4}
+    {}
+};
 
 int main(){
+    float w = rand_float();
     printf("Hello World\n");
 
     return 0;
