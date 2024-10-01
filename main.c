ffb85933336e5592a4f6819f147471ee4a6d5c90
Farhan <loki4444protonmail.com>
2024-10-01 16:19:52 +0600

    start....

--- main.c
+++ main.c
@@ -2,10 +2,15 @@
 
 // this the training data for say...
 float train[][2]={
-    {1,2}
-    {2,4}
-    {}
+    {1,2},
+    {2,4},
+    {4,8},
 };
+
+float rand_float(void){
+    RAND_MAX
+    rand();
+}
 
 int main(){
     float w = rand_float();
