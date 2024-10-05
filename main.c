a5ccac6477a40174126164a52dc215890f874405
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-10-06 00:14:30 +0600

    added a few lines and tested it....

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <complex.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 // this the training data for say...
 float train[][2]={
+    {0,0},
     {1,2},
     {2,4},
     {4,8},
     {5,10}
 };
+
+#define train_count (sizeof(train)/sizeof(train[0]))
 
 float rand_float(void){
     
@@ -17,8 +20,15 @@ float rand_float(void){
 }
 
 int main(){
-    float w = rand_float();
-    printf("%f\n",w);
+    // srand(time(0));
+    srand(69);
+    // c=a*b
+    float w = rand_float()*10.0f;
+    for(size_t i=0;i<train_count;i++){
+        float x=train [i][0];
+        float y=x*w;
+        printf("Actual: %f, Expected: %f\n",y,train[i][0]);
+    }
 
     return 0;
 }
