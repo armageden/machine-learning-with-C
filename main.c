e5a32ff8f74afbde0342080f56697cac37995592
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-10-05 23:35:02 +0600

    me lazy....

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
+#include <complex.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 // this the training data for say...
 float train[][2]={
     {1,2},
     {2,4},
     {4,8},
+    {5,10}
 };
 
 float rand_float(void){
-    RAND_MAX
-    rand();
+    
+   return (float) rand()/(float)RAND_MAX;
 }
 
 int main(){
     float w = rand_float();
-    printf("Hello World\n");
+    printf("%f\n",w);
 
     return 0;
 }
