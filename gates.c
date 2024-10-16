632689c61c8c7e63fa542a89cae542b5c432325d
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-10-16 07:30:44 +0600

    added a bias for better results...

--- gates.c
+++ gates.c
@@ -1,13 +1,11 @@
 #include <complex.h>
-#include <time.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // sigmoid-funtion
-float sigmoidf(float x){
-  return 1.f/(1.f+expf(-x));
-}
+float sigmoidf(float x) { return 1.f / (1.f + expf(-x)); }
 // OR-gate
 // this is the training data for say...
 float train[][3] = {{0, 0, 0}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1}};
@@ -15,13 +13,12 @@ float train[][3] = {{0, 0, 0}, {1, 0, 1}, {0, 1, 1}, {1, 1, 1}};
 #define train_count (sizeof(train) / sizeof(train[0]))
 
 // cost function
-float cost(float w1, float w2) {
+float cost(float w1, float w2, float bias) {
   float result = 0.0f;
-
   for (size_t i = 0; i < train_count; i++) {
     float x1 = train[i][0];
     float x2 = train[i][1];
-    float y = sigmoidf(x1 * w1 + x2 * w2);
+    float y = sigmoidf(x1 * w1 + x2 * w2 + bias);
     float d = y - train[i][2];
     result += d * d;
   }
@@ -35,24 +32,27 @@ int main(void) {
   srand(time(0));
   float eps = 1e-1;
   float rate = 1e-1;
-  float w1 = rand_float()*10-5;
-  float w2 = rand_float()*10-5;
-  
-  for (size_t i = 0; i < 1000; ++i) {
+  float w1 = rand_float();
+  float w2 = rand_float();
+  float bias = rand_float();
 
-    float c = cost(w1, w2);
-    printf("w1= %f , w2= %f,c= %f\n", w1, w2, c);
-    float dw1 = (cost(w1 + eps, w2) - c) / eps;
-    float dw2 = (cost(w1, w2 + eps) - c) / eps;
+  for (size_t i = 0; i < 1000000; ++i) {
+
+    float c = cost(w1, w2, bias);
+    printf("w1 = %f , w2 = %f, bias = %f, c = %f\n", w1, w2, bias, c);
+    float dw1 = (cost(w1 + eps, w2, bias) - c) / eps;
+    float dw2 = (cost(w1, w2 + eps, bias) - c) / eps;
+    float db = (cost(w1, w2, bias + eps) - c) / eps;
     w1 -= rate * dw1;
     w2 -= rate * dw2;
+    bias -= rate * db;
   }
-  printf("w1= %f , w2= %f,c= %f\n", w1, w2, cost( w1,  w2));
+  printf("w1 = %f , w2 = %f, bias = %f, c = %f\n", w1, w2,bias, cost(w1, w2, bias));
 
-  for (size_t i=0; i<2; ++i) {
-  for (size_t j=0; j<2;++j) {
-  printf("%zu | %zu = %f\n",i,j,sigmoidf(i * w1 + j * w2));
-  }
+  for (size_t i = 0; i < 2; ++i) {
+    for (size_t j = 0; j < 2; ++j) {
+      printf("%zu | %zu = %f\n", i, j, sigmoidf(i * w1 + j * w2));
+    }
   }
   return 0;
 }
