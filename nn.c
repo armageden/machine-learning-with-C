6a9135ce21eb4e95b151c4b7e1f3e043101f51c2
arm-td93 <86878487+armageden@users.noreply.github.com>
2024-12-16 23:51:44 +0600

    added seed for the randomizer

--- nn.c
+++ nn.c
@@ -1,7 +1,10 @@
 #include "nn.h"
+#include <time.h>
 #define NN_IMPLIMENTATION
 int main(void){
+    srand(time(0));
     Mat m=mat_alloc(10, 10);
+    mat_rand(m,0,10);
     mat_print(m);
     return 0;
 }
--- nn.h
+++ nn.h
@@ -9,6 +9,7 @@
 
 // Mat di={.rows=4, .cols=2 ,.stride=3 .es=&d[0]};
 // Mat do={.rows=4, .cols=1 ,.stride=3 .es=&d[3]};
+// #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
 
@@ -29,14 +30,18 @@ typedef struct {
 
 #define MAT_AT(m, i, j) (m).es[(i) * (m).cols + (j)]
 
+float rand_float(void);
+
 Mat mat_alloc(size_t rows, size_t cols);
-void mat_rand(Mat m);
+void mat_rand(Mat m, float low, float high);
 void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst, Mat a);
 void mat_print(Mat m);
 #endif // NN_H_
 
 #ifndef NN_IMPLIMENTATION
+float rand_float(void) { return (float)rand() / (float)RAND_MAX; }
+
 Mat mat_alloc(size_t rows, size_t cols) {
   Mat m;
   m.rows = rows;
@@ -51,8 +56,13 @@ void mat_dot(Mat dst, Mat a, Mat b) {
   (void)b;
 }
 void mat_sum(Mat dst, Mat a) {
-  (void)dst;
-  (void)a;
+  NN_ASSERT(dst.rows == a.rows);
+  NN_ASSERT(dst.cols == a.cols);
+  for (size_t i = 0; i < dst.rows; ++i) {
+    for (size_t j = 0; j < dst.rows; ++j) {
+      MAT_AT(dst, i, j) += MAT_AT(a, i, j);
+    }
+  }
 }
 
 void mat_print(Mat m) {
@@ -65,9 +75,10 @@ void mat_print(Mat m) {
   }
 }
 
-void mat_rand(Mat m) {
+void mat_rand(Mat m, float low, float high) { // need to randomize this stuff
   for (size_t i = 0; i < m.rows; ++i) {
     for (size_t j = 0; j < m.cols; ++j) {
+      MAT_AT(m, i, j) = rand_float() * (high - low) + low;
     }
   }
 }
